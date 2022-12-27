a22278f1823fc28f92a79a620950ebacf9d2d53a
bacevedo12 <bacevedo85@hotmail.com>
2022-12-27 11:44:10 -0300

    creo carpeta frontend y backend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Inicio from './Frontend/components/Inicio/Inicio';
-import Menu  from './Frontend/components/Menu/Menu';
-import Iniciarsesion from './Frontend/components/Iniciarsesion/Iniciarsesion';
-import Miperfil from './Frontend/components/Miperfil/Miperfil';
-import Registro from './Frontend/components/Registro/Registro';
-import NavBarComercio from './Frontend/layouts/Navbar';
+import Inicio from './components/Inicio/Inicio';
+import Menu  from './components/Menu/Menu';
+import Iniciarsesion from './components/Iniciarsesion/Iniciarsesion';
+import Miperfil from './components/Miperfil/Miperfil';
+import Registro from './components/Registro/Registro';
+import NavBarComercio from './layouts/Navbar';
 
 
 function App() {
