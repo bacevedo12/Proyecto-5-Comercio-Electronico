2dee4469d2cf73d62a73c9f3933db54130182309
Camila-FR <camilafloresramirez@gmail.com>
2022-12-29 18:19:05 -0300

    avance menu2

--- Frontend/src/components/Menu/Menu.js
+++ Frontend/src/components/Menu/Menu.js
@@ -2,22 +2,31 @@
 import './Menu.css';
 import menu from './Data.js'
 import Platos from "./Platos.js";
+import Instrucciones from './Instrucciones.js';
+import Imagen_Fondo from '../imagenes/foto-comidaadomicilio.jpg'
 
 
 const platoCasero = menu.filter((element) => element.category === "Casero");
 console.log (platoCasero)
+
 const Menu = () => {
     return (
-        
-
         <div className="fondo">
-            <h1 className='Titulo'>Menu</h1>
-            <hr></hr>
-            <h1 className='Titulo'>Platos Caseros</h1>
-                <div className='d-flex justify-content-center'>            
-                    <Platos items={platoCasero}/>
-                </div> 
-
+             <div className='container p-0'>
+                               
+                <div className='ContenidoPlatos'>
+                    <h1 className='Titulo'>Platos</h1>
+                    <hr></hr>
+                        <Instrucciones/>
+                    <hr></hr>
+                    <h1 className='Titulo'>Platos Caseros</h1>
+                    <img src={Imagen_Fondo} alt='imagen de fondo' className='ImagenFondo'></img>
+                    
+                        <div className='d-flex justify-content-center'>            
+                            <Platos items={platoCasero}/>
+                        </div> 
+              </div>  
+            </div>
         </div>
     )
 }
