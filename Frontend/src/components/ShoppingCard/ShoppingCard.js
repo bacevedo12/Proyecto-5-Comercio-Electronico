a72ba68c1a2f55eaa8db321d21823c91d01f5773
bacevedo12 <bacevedo85@hotmail.com>
2023-01-18 10:46:08 -0300

    agregoshoppingCard

---
+++ Frontend/src/components/ShoppingCard/ShoppingCard.js
@@ -0,0 +1,51 @@
+import { useReducer } from "react";
+import "./shoppingcard.css"
+import { shoppingInitialState, shoppingReducer } from "./reducers/shoppingReducer.js";
+import ProductItem from "./ProductItem.js";
+import CartItem from "./cartItem/CartItem.js"
+import { TYPES } from "./actions/shoppingAction.js";
+
+function ShoppingCard () {
+    const [state,dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+    const { products,cart} = state;
+
+    const addToCart = (id) => {
+        console.log(id);
+        dispatch({type:TYPES.ADD_TO_CART,payload:id});
+    }
+
+    const delFromCart = (id,all = false) => {
+        console.log(id,all);
+        if(all) {
+            dispatch({type:TYPES.REMOVE_ALL_FROM_CART,paylod:id})
+        }else {
+            dispatch({type:TYPES.REMOVE_ONE_FROM_CART,paylod:id})
+        }
+    };
+
+    const clearCart = () => {
+        dispatch({type:TYPES.CLEAR_CART});
+    };
+
+    return (
+        <section>
+            <h1>Carrito compra</h1>
+            <h3>Productos</h3>
+            <article className="box grid-responsive">
+                {products.map((product) => (
+                <ProductItem key={product.id} data={product} addToCart={addToCart}/>
+                ))}
+            </article>
+            <h3>Carrito</h3>
+            <article className="box">
+                <button onClick={clearCart}>Limpiar Carrito</button>
+                {
+                    cart.map((item,index) => 
+                    <CartItem key={index}data={item}delFromCart={delFromCart}/>)
+                }
+            </article>
+        </section>
+    )
+}
+
+export default ShoppingCard;
