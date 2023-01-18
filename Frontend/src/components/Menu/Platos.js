fdb0f7550b2f8450afb018f5dc54a7f2a1d886ad
Camila-FR <camilafloresramirez@gmail.com>
2023-01-18 00:55:49 -0300

    menu

--- Frontend/src/components/Menu/Platos.js
+++ Frontend/src/components/Menu/Platos.js
@@ -11,17 +11,17 @@ const Platos = ({items}) =>{
     return (
       <div className="row justify-content-center">
         {items.map((items, key)=>{       
-        const {title, img, desc, price} = items
+        const {title, img, price} = items
         return (        
             <Card key={key} className="col-xs-12 col-lg-3 m-3  colorcard" style={{ width: '18rem' }} >
                 <Card.Body>
                 <Card.Title className='titulo'><h3>{title}</h3></Card.Title>
-                <Card.Img className='img_menu' variant="top" src={img} alt={title}  />
+                <Card.Img className='img_menu py-2' variant="top" src={img} alt={title}  />
                 <Card.Text className='desc'>
-                  {desc} <br/>
-                  {price}
+                  <h5>Precio</h5>
+                   {price}
                 </Card.Text>
-                <button type="button" className="btn btn-info">Agregar</button>
+                <button type="button" className="boton">Agregar</button>
               </Card.Body>
             </Card>
         )
